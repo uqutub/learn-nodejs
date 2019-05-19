1ea81d62216cb9c12ba0887b44c635f27095ed8c
Yousuf Qutubuddin <usuf52@gmail.com>
2019-05-19 10:15:59 +0500

    update 06 url file

--- 06_url.js
+++ 06_url.js
@@ -7,8 +7,7 @@ http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   console.log('before ', req.url);
   var q = url.parse(req.url, true).query;
-  console.log('after ', req.url);
-  console.log('q ', q);
+  console.log('q after ', q);
   var txt = q.year + " " + q.month;
   res.end(txt);
 }).listen(8080);
